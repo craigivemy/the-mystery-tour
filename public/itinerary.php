490cc08996d9454962f686236b3524bf9312a06e
Craig Ivemy <craig@strategic-ic.co.uk>
2019-05-31 12:40:54 +0100

    Itinerary heading

--- public/itinerary.php
+++ public/itinerary.php
@@ -1,3 +1,10 @@
 <?php include_once '../src/partials/header.php'; ?>
 <div class="itinerary">
+    <div class="container">
+        <div class="row">
+            <div class="col-10 col-md-8 mx-auto itinerary__sheet">
+                <h1 class="itinerary__sheet--title">Itinerary</h1>
+            </div>
+        </div>
+    </div>
 </div>
